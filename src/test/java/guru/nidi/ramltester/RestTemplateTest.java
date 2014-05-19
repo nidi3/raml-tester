3e6df717d0cb28ee76150db5db320725c4510ede
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2014-05-19 23:27:16 +0200

    add license headers

--- src/test/java/guru/nidi/ramltester/RestTemplateTest.java
+++ src/test/java/guru/nidi/ramltester/RestTemplateTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) ${project.inceptionYear} Stefan Niederhauser (nidin@gmx.ch)
+ * Copyright (C) 2014 Stefan Niederhauser (nidin@gmx.ch)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
