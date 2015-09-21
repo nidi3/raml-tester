13179e76b29c8581087d951bc394fa651a5da07c
Stefan Niederhauser <ghuder5@gmx.ch>
2015-09-21 22:27:40 +0200

    reproduce #52 with test case, but can only be fixed in raml-java-parser

---
+++ src/test/java/guru/nidi/ramltester/IncludeTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2014 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package guru.nidi.ramltester;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.raml.model.Action;
+import org.raml.model.ActionType;
+import org.raml.model.Raml;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+
+
+/**
+ * Wait for https://github.com/raml-org/raml-java-parser/issues/80 to be fixed.
+ */
+public class IncludeTest extends HighlevelTestBase {
+    private RamlDefinition api = RamlLoaders.fromClasspath(getClass()).load("include.raml");
+
+    @Test
+    @Ignore
+    public void load() throws Exception {
+        final Raml raml = api.getRaml();
+        final Action get = raml.getResource("/site").getAction(ActionType.GET);
+        assertNull(get.getQueryParameters().get("string.json"));
+        assertNotEquals("string.json", get.getResponses().get("200").getBody().get("application/json").getExample());
+        assertNotEquals("string.json", get.getResponses().get("201").getBody().get("application/json").getSchema());
+        assertNotEquals("string.json", get.getResponses().get("201").getBody().get("application/json").getExample());
+    }
+
+
+}
