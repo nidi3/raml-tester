245d1c981464f1d48401b915f65d5c1e807d8293
Stefan Niederhauser <ghuder5@gmx.ch>
2016-01-07 17:50:38 +0100

    use code-assert-0.0.2

--- src/test/java/guru/nidi/ramltester/CodeAnalysisTest.java
+++ src/test/java/guru/nidi/ramltester/CodeAnalysisTest.java
@@ -16,8 +16,9 @@
 package guru.nidi.ramltester;
 
 import edu.umd.cs.findbugs.Priorities;
-import guru.nidi.codeassert.AnalyzerConfig;
-import guru.nidi.codeassert.PackageCollector;
+import guru.nidi.codeassert.config.AnalyzerConfig;
+import guru.nidi.codeassert.config.In;
+import guru.nidi.codeassert.config.PackageCollector;
 import guru.nidi.codeassert.dependency.DependencyRule;
 import guru.nidi.codeassert.dependency.DependencyRuler;
 import guru.nidi.codeassert.dependency.DependencyRules;
@@ -90,24 +91,19 @@ public class CodeAnalysisTest {
     @Test
     public void findBugs() {
         final BugCollector collector = BugCollector.simple(null, Priorities.NORMAL_PRIORITY)
-                .because("I don't agree").ignore(
-                        "SBSC_USE_STRINGBUFFER_CONCATENATION").generally()
-                .because("is in test").ignore(
-                        "SIC_INNER_SHOULD_BE_STATIC_ANON", "SE_NO_SERIALVERSIONID", "DM_DEFAULT_ENCODING", "BC_UNCONFIRMED_CAST").in(
-                        "*Test$*")
-                .because("is in test").ignore(
-                        "ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD", "DM_DEFAULT_ENCODING", "UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR").in(
-                        "*Test")
-                //TODO
-                .because("TODO").ignore(
-                        "DM_DEFAULT_ENCODING").in(
-                        "RestAssuredRamlRequest", "ServletRamlRequest", "ServletRamlResponse")
-                .because("arrays are only used internally").ignore(
-                        "EI_EXPOSE_REP", "EI_EXPOSE_REP2").in(
-                        "*Response", "*Request")
-                .because("it's class private and only used in 1 occasion").ignore(
-                        "EQ_COMPARETO_USE_OBJECT_EQUALS").in(
-                        "CheckerHelper$ResourceMatch");
+                .because("I don't agree",
+                        In.everywhere().ignore("SBSC_USE_STRINGBUFFER_CONCATENATION"))
+                .because("it's in test",
+                        In.loc("*Test$*")
+                                .ignore("SIC_INNER_SHOULD_BE_STATIC_ANON", "SE_NO_SERIALVERSIONID", "DM_DEFAULT_ENCODING", "BC_UNCONFIRMED_CAST"),
+                        In.loc("*Test")
+                                .ignore("ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD", "DM_DEFAULT_ENCODING", "UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR"))
+                .because("TODO",                 //TODO
+                        In.locs("RestAssuredRamlRequest", "ServletRamlRequest", "ServletRamlResponse").ignore("DM_DEFAULT_ENCODING"))
+                .because("arrays are only used internally",
+                        In.locs("*Response", "*Request").ignore("EI_EXPOSE_REP", "EI_EXPOSE_REP2"))
+                .because("it's class private and only used in 1 occasion",
+                        In.loc("CheckerHelper$ResourceMatch").ignore("EQ_COMPARETO_USE_OBJECT_EQUALS"));
         final FindBugsAnalyzer analyzer = new FindBugsAnalyzer(withTest, collector);
         assertThat(analyzer, findsNoBugs());
     }
@@ -115,59 +111,37 @@ public class CodeAnalysisTest {
     @Test
     public void pmd() {
         final ViolationCollector collector = ViolationCollector.simple(RulePriority.MEDIUM)
-                .because("makes no sense").ignore(
-                        "JUnitSpelling").generally()
-                .because("does not understand constants (logger is NOT)").ignore(
-                        "VariableNamingConventions").generally()
-                .because("I don't agree").ignore(
-                        "UncommentedEmptyMethodBody", "AvoidFieldNameMatchingMethodName", "AvoidFieldNameMatchingTypeName",
-                        "AbstractNaming", "UncommentedEmptyConstructor", "SimplifyStartsWith", "EmptyMethodInAbstractClassShouldBeAbstract",
-                        "AvoidSynchronizedAtMethodLevel", "UseStringBufferForStringAppends").generally()
-                .because("I don't agree").ignore(
-                        "ConfusingTernary").in(
-                        "SecurityExtractor$SchemeFinder")
-                .because("I don't agree").ignore(
-                        "NPathComplexity").in(
-                        "UriComponents#getServer")
-                .because("arrays are only used internally").ignore(
-                        "MethodReturnsInternalArray", "ArrayIsStoredDirectly").in(
-                        "*Response", "*Request")
-                .because("not urgent and too many occasions").ignore(
-                        "AvoidInstantiatingObjectsInLoops", "JUnitAssertionsShouldIncludeMessage", "JUnitTestContainsTooManyAsserts",
-                        "MethodArgumentCouldBeFinal").generally()
-                .because("it's plain wrong").ignore(
-                        "ClassWithOnlyPrivateConstructorsShouldBeFinal").in(
-                        "UsageCollector")
-                .because("it's checked and correct").ignore(
-                        "CompareObjectsWithEquals").in(
-                        "RelativeJsonSchemaAwareRamlDocumentBuilder", "MediaType", "ServletRamlMessageTest")
-                .because("it's checked and correct").ignore(
-                        "PreserveStackTrace").in(
-                        "JsRegex", "MediaType")
-                .because("it's checked and correct").ignore(
-                        "AvoidCatchingGenericException").in(
-                        "JsRegex")
-                .because("it's checked and correct").ignore(
-                        "JUnitTestsShouldIncludeAssert").in(
-                        UriTest.class, ParameterCheckerTest.class, MediaTypeTest.class)
-                .because("it's style").ignore(
-                        "CollapsibleIfStatements").in(
-                        "RamlValidatorChecker")
-                //TODO
-                .because("TODO").ignore(
-                        "GodClass").in(
-                        "ParameterChecker", "Usage", "MediaType")
-                //TODO
-                .because("TODO").ignore(
-                        "CyclomaticComplexity", "NPathComplexity").in(
-                        "VariableMatcher", "MediaType")
-                //TODO
-                .because("TODO").ignore(
-                        "AvoidDeeplyNestedIfStmts").in(
-                        "ContentNegotiationChecker")
-                .because("is in test").ignore(
-                        "AvoidDuplicateLiterals", "SignatureDeclareThrowsException", "TooManyStaticImports", "AvoidDollarSigns").in(
-                        "*Test", "*Test$*");
+                .because("makes no sense",
+                        In.everywhere().ignore("JUnitSpelling"))
+                .because("does not understand constants (logger is NOT)",
+                        In.everywhere().ignore("VariableNamingConventions"))
+                .because("I don't agree",
+                        In.everywhere().ignore(
+                                "UncommentedEmptyMethodBody", "AvoidFieldNameMatchingMethodName", "AvoidFieldNameMatchingTypeName",
+                                "AbstractNaming", "UncommentedEmptyConstructor", "SimplifyStartsWith", "EmptyMethodInAbstractClassShouldBeAbstract",
+                                "AvoidSynchronizedAtMethodLevel", "UseStringBufferForStringAppends"),
+                        In.loc("SecurityExtractor$SchemeFinder").ignore("ConfusingTernary"),
+                        In.loc("UriComponents#getServer").ignore("NPathComplexity"))
+                .because("arrays are only used internally",
+                        In.locs("*Response", "*Request").ignore("MethodReturnsInternalArray", "ArrayIsStoredDirectly"))
+                .because("not urgent and too many occasions",
+                        In.everywhere().ignore(
+                                "AvoidInstantiatingObjectsInLoops", "JUnitAssertionsShouldIncludeMessage", "JUnitTestContainsTooManyAsserts", "MethodArgumentCouldBeFinal"))
+                .because("it's plain wrong",
+                        In.loc("UsageCollector").ignore("ClassWithOnlyPrivateConstructorsShouldBeFinal"))
+                .because("it's checked and correct",
+                        In.locs("RelativeJsonSchemaAwareRamlDocumentBuilder", "MediaType", "ServletRamlMessageTest").ignore("CompareObjectsWithEquals"),
+                        In.locs("JsRegex", "MediaType").ignore("PreserveStackTrace"),
+                        In.loc("JsRegex").ignore("AvoidCatchingGenericException"),
+                        In.classes(UriTest.class, ParameterCheckerTest.class, MediaTypeTest.class).ignore("JUnitTestsShouldIncludeAssert"))
+                .because("it's style",
+                        In.loc("RamlValidatorChecker").ignore("CollapsibleIfStatements"))
+                .because("TODO",                 //TODO
+                        In.locs("ParameterChecker", "Usage", "MediaType").ignore("GodClass"),
+                        In.locs("VariableMatcher", "MediaType").ignore("CyclomaticComplexity", "NPathComplexity"),
+                        In.loc("ContentNegotiationChecker").ignore("AvoidDeeplyNestedIfStmts"))
+                .because("is in test",
+                        In.locs("*Test", "*Test$*").ignore("AvoidDuplicateLiterals", "SignatureDeclareThrowsException", "TooManyStaticImports", "AvoidDollarSigns"));
         final PmdAnalyzer analyzer = new PmdAnalyzer(withTest, collector)
                 .withRuleSets(basic(), braces(), design(), exceptions(), imports(), junit(),
                         optimizations(), strings(), sunSecure(), typeResolution(), unnecessary(), unused(),
@@ -179,20 +153,18 @@ public class CodeAnalysisTest {
 
     @Test
     public void cpd() {
-        final MatchCollector collector = new MatchCollector()
-                .because("there's no common superclass").ignore(
-                        "DelegatingServletOutputStream", "DelegatingWriter")
-                //TODO
-                .because("TODO").ignore(
-                        "SavingOutputStream", "DelegatingServletOutputStream", "DelegatingWriter")
-                .because("java...").ignore(
-                        "UsageCollector", "SecurityExtractor")
-                .because("Similar but not same").ignore(
-                        "*Request", "*Response")
-                .because("Similar but not same").ignore(
-                        RamlHttpClient.class)
-                .because("Equals...").ignore(
-                        "RamlViolations", "Values");
+        final MatchCollector collector = MatchCollector.simple()
+                .because("there's no common superclass",
+                        In.locs("DelegatingServletOutputStream", "DelegatingWriter").ignoreAll())
+                .because("TODO",                 //TODO
+                        In.locs("SavingOutputStream", "DelegatingServletOutputStream", "DelegatingWriter").ignoreAll())
+                .because("java...",
+                        In.locs("UsageCollector", "SecurityExtractor").ignoreAll())
+                .because("Similar but not same",
+                        In.locs("*Request", "*Response").ignoreAll(),
+                        In.clazz(RamlHttpClient.class).ignoreAll())
+                .because("Equals...",
+                        In.locs("RamlViolations", "Values").ignoreAll());
         final CpdAnalyzer analyzer = new CpdAnalyzer(withoutTest, 35, collector);
         assertThat(analyzer, hasNoDuplications());
     }
