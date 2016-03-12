d5441b39593ef07a02d614c94b218fada0e5e0cf
Stefan Niederhauser <ghuder5@gmx.ch>
2016-03-13 00:44:53 +0100

    use code-assert-0.0.3

--- src/test/java/guru/nidi/ramltester/CodeAnalysisTest.java
+++ src/test/java/guru/nidi/ramltester/CodeAnalysisTest.java
@@ -24,6 +24,7 @@ import guru.nidi.codeassert.dependency.DependencyRules;
 import guru.nidi.codeassert.findbugs.BugCollector;
 import guru.nidi.codeassert.findbugs.FindBugsAnalyzer;
 import guru.nidi.codeassert.findbugs.FindBugsResult;
+import guru.nidi.codeassert.junit.CodeAssertTest;
 import guru.nidi.codeassert.model.ModelAnalyzer;
 import guru.nidi.codeassert.model.ModelResult;
 import guru.nidi.codeassert.pmd.*;
@@ -31,95 +32,48 @@ import guru.nidi.ramltester.core.ParameterCheckerTest;
 import guru.nidi.ramltester.httpcomponents.RamlHttpClient;
 import guru.nidi.ramltester.util.MediaTypeTest;
 import net.sourceforge.pmd.RulePriority;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static guru.nidi.codeassert.config.PackageCollector.allPackages;
-import static guru.nidi.codeassert.dependency.DependencyMatchers.*;
-import static guru.nidi.codeassert.findbugs.FindBugsMatchers.hasNoBugs;
-import static guru.nidi.codeassert.pmd.PmdMatchers.hasNoDuplications;
-import static guru.nidi.codeassert.pmd.PmdMatchers.hasNoPmdViolations;
+import static guru.nidi.codeassert.junit.CodeAssertMatchers.matchesExactly;
 import static guru.nidi.codeassert.pmd.Rulesets.*;
 import static org.junit.Assert.assertThat;
 
 /**
  *
  */
-public class CodeAnalysisTest {
-    private static AnalyzerConfig withTest, withoutTest;
-
-    private static ModelResult modelResult;
-    private static FindBugsResult findBugsResult;
-    private static PmdResult pmdResult;
-    private static CpdResult cpdResult;
-
-    @BeforeClass
-    public static void init() {
-        withTest = AnalyzerConfig.mavenMainAndTestClasses()
-                .collecting(allPackages().including("guru.nidi.ramltester").excludingRest());
-        withoutTest = AnalyzerConfig.mavenMainClasses()
-                .collecting(allPackages().including("guru.nidi.ramltester").excludingRest());
-
-        modelResult = new ModelAnalyzer(withoutTest).analyze();
-        findBugsResult = findBugs();
-        pmdResult = pmd();
-        cpdResult = cpd();
-    }
-
+public class CodeAnalysisTest extends CodeAssertTest {
     @Test
     public void dependencies() {
         class GuruNidiRamltester implements DependencyRuler {
             DependencyRule $self, core, httpcomponents, restassured, junit, validator, model, servlet, spring, jaxrs, util;
 
             public void defineRules() {
-                $self.mayDependUpon(model, core, servlet, httpcomponents, restassured, spring, jaxrs, validator, junit, util);
-                core.mayDependUpon(model, util);
-                util.mayDependUpon(model);
-                servlet.mayDependUpon(model, util, core);
-                httpcomponents.mayDependUpon(model, util, core);
-                restassured.mayDependUpon(model, core);
-                junit.mayDependUpon(util, core);
-                validator.mayDependUpon(util, core);
-                spring.mayDependUpon(model, util, core, servlet);
-                jaxrs.mayDependUpon(model, util, core);
+                $self.mayUse(model, core, servlet, httpcomponents, restassured, spring, jaxrs, validator, junit, util);
+                core.mayUse(model, util);
+                util.mayUse(model);
+                servlet.mayUse(model, util, core);
+                httpcomponents.mayUse(model, util, core);
+                restassured.mayUse(model, core);
+                junit.mayUse(util, core);
+                validator.mayUse(util, core);
+                spring.mayUse(model, util, core, servlet);
+                jaxrs.mayUse(model, util, core);
             }
         }
-
-        final DependencyRules rules = DependencyRules.denyAll().withRules(new GuruNidiRamltester());
-        assertThat(modelResult, matchesExactly(rules));
+//TODO dependencies to externals (spring, httpcomponents etc.)
+        final DependencyRules rules = DependencyRules.denyAll()
+                .withExternals("java*", "org*", "com*", "guru.nidi.loader*")
+                .withRules(new GuruNidiRamltester());
+        assertThat(modelResult(), matchesExactly(rules));
     }
 
-    @Test
-    public void noCircularDependencies() {
-        assertThat(modelResult, hasNoCycles());
+    @Override
+    protected ModelResult analyzeModel() {
+        return new ModelAnalyzer(AnalyzerConfig.maven().main()).analyze();
     }
 
-    @Test
-    public void noFindBugs() {
-        assertThat(findBugsResult, hasNoBugs());
-    }
-
-    @Test
-    public void noFindBugsUnusedActions() {
-        assertThat(findBugsResult, hasNoUnusedActions());
-    }
-
-    @Test
-    public void noPmdViolations() {
-        assertThat(pmdResult, hasNoPmdViolations());
-    }
-
-    @Test
-    public void noPmdUnusedActions() {
-        assertThat(pmdResult, hasNoUnusedActions());
-    }
-
-    @Test
-    public void noDuplications() {
-        assertThat(cpdResult, hasNoDuplications());
-    }
-
-    private static FindBugsResult findBugs() {
+    @Override
+    protected FindBugsResult analyzeFindBugs() {
         final BugCollector collector = new BugCollector().minPriority(Priorities.NORMAL_PRIORITY)
                 .because("I don't agree",
                         In.everywhere().ignore("SBSC_USE_STRINGBUFFER_CONCATENATION"))
@@ -136,10 +90,11 @@ public class CodeAnalysisTest {
                         In.loc("guru.nidi.ramltester.snippets.*").ignoreAll())
                 .because("it's class private and only used in 1 occasion",
                         In.loc("CheckerHelper$ResourceMatch").ignore("EQ_COMPARETO_USE_OBJECT_EQUALS"));
-        return new FindBugsAnalyzer(withTest, collector).analyze();
+        return new FindBugsAnalyzer(AnalyzerConfig.maven().mainAndTest(), collector).analyze();
     }
 
-    private static PmdResult pmd() {
+    @Override
+    protected PmdResult analyzePmd() {
         final ViolationCollector collector = new ViolationCollector().minPriority(RulePriority.MEDIUM)
                 .because("makes no sense",
                         In.everywhere().ignore("JUnitSpelling"))
@@ -174,7 +129,7 @@ public class CodeAnalysisTest {
                         In.loc("guru.nidi.ramltester.snippets.*").ignoreAll())
                 .because("is in test",
                         In.locs("*Test", "*Test$*").ignore("AvoidDuplicateLiterals", "SignatureDeclareThrowsException", "TooManyStaticImports", "AvoidDollarSigns"));
-        return new PmdAnalyzer(withTest, collector)
+        return new PmdAnalyzer(AnalyzerConfig.maven().mainAndTest(), collector)
                 .withRuleSets(basic(), braces(), design(), exceptions(), imports(), junit(),
                         optimizations(), strings(), sunSecure(), typeResolution(), unnecessary(), unused(),
                         codesize().tooManyMethods(35),
@@ -183,19 +138,20 @@ public class CodeAnalysisTest {
                 .analyze();
     }
 
-    private static CpdResult cpd() {
+    @Override
+    protected CpdResult analyzeCpd() {
         final MatchCollector collector = new MatchCollector()
                 .because("there's no common superclass",
                         In.locs("DelegatingServletOutputStream", "DelegatingWriter").ignoreAll())
                 .because("TODO",                 //TODO
                         In.locs("SavingOutputStream", "DelegatingServletOutputStream", "DelegatingWriter").ignoreAll())
-                .because("java...",
-                        In.locs("UsageCollector", "SecurityExtractor").ignoreAll())
+                .just(
+                        In.loc("UsageCollector").ignore("static final UsageCollector"),
+                        In.loc("SecurityExtractor").ignore("for (final SecurityScheme scheme : schemes)"),
+                        In.everywhere().ignore("public boolean equals(Object o) {"))
                 .because("Similar but not same",
                         In.locs("*Request", "*Response").ignoreAll(),
-                        In.clazz(RamlHttpClient.class).ignoreAll())
-                .because("Equals...",
-                        In.locs("RamlViolations", "Values").ignoreAll());
-        return new CpdAnalyzer(withoutTest, 35, collector).analyze();
+                        In.clazz(RamlHttpClient.class).ignoreAll());
+        return new CpdAnalyzer(AnalyzerConfig.maven().main(), 35, collector).analyze();
     }
 }
