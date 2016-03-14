4513c2539e4e646060414d5cd5bf1c611e5635aa
Stefan Niederhauser <ghuder5@gmx.ch>
2016-03-14 03:43:56 +0100

    add license

--- src/test/java/guru/nidi/ramltester/validator/MyTypeValidator.java
+++ src/test/java/guru/nidi/ramltester/validator/MyTypeValidator.java
@@ -1,22 +1,18 @@
 /*
- * Copyright (c) 2014, Francis Galiegue (fgaliegue@gmail.com)
+ * Copyright (C) 2014 Stefan Niederhauser (nidin@gmx.ch)
  *
- * This software is dual-licensed under:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
- *   later version;
- * - the Apache Software License (ASL) version 2.0.
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * The text of this file and of both licenses is available at the root of this
- * project or, if you have the jar distribution, in directory META-INF/, under
- * the names LGPL-3.0.txt and ASL-2.0.txt respectively.
- *
- * Direct link to the sources:
- *
- * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
- * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package guru.nidi.ramltester.validator;
 
 import com.fasterxml.jackson.databind.JsonNode;
