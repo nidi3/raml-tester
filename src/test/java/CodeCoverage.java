708d5d1ac3cf023034408baf733717df49df96d4
Stefan Niederhauser <ghuder5@gmx.ch>
2016-04-25 00:02:34 +0200

    use CodeCoverage

---
+++ src/test/java/CodeCoverage.java
@@ -0,0 +1,27 @@
+import guru.nidi.codeassert.config.For;
+import guru.nidi.codeassert.jacoco.CoverageCollector;
+import guru.nidi.codeassert.jacoco.JacocoAnalyzer;
+import org.junit.Test;
+
+import static guru.nidi.codeassert.jacoco.CoverageType.*;
+import static guru.nidi.codeassert.junit.CodeAssertMatchers.hasEnoughCoverage;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ */
+public class CodeCoverage {
+    @Test
+    public void coverage() {
+        //TODO 75,75,75 should be the goal
+        final JacocoAnalyzer analyzer = new JacocoAnalyzer(new CoverageCollector(BRANCH, LINE, METHOD)
+                .just(For.global().setMinima(75, 75, 75))
+                .just(For.allPackages().setMinima(75, 75, 75))
+                .just(For.packge("*.httpcomponents").setMinima(50, 60, 60))
+                .just(For.packge("*.jaxrs").setMinima(50, 60, 50))
+                .just(For.packge("*.servlet").setMinima(70, 60, 75))
+                .just(For.packge("*.validator").setMinima(70, 75, 75))
+        );
+        assertThat(analyzer.analyze(), hasEnoughCoverage());
+    }
+}
---
+++ src/test/java/guru/nidi/ramltester/model/ValuesTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2014 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package guru.nidi.ramltester.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ *
+ */
+public class ValuesTest {
+    private Values values;
+
+    @Before
+    public void init() {
+        final Map<String, String[]> vals = new HashMap<>();
+        vals.put("a", new String[]{"1"});
+        vals.put("b", new String[]{"1", "2"});
+        values = new Values(vals);
+    }
+
+    @Test
+    public void size() {
+        assertEquals(2, values.size());
+    }
+
+    @Test
+    public void get() {
+        assertEquals(Arrays.asList("1"), values.get("a"));
+        assertEquals(Arrays.asList("1", "2"), values.get("b"));
+        assertNull(values.get("c"));
+    }
+
+    @Test
+    public void addValue() {
+        values.addValue("a", "3");
+        values.addValue("c", "3");
+        assertEquals(Arrays.asList("1", "3"), values.get("a"));
+        assertEquals(Arrays.asList("3"), values.get("c"));
+    }
+
+    @Test
+    public void addValues() {
+        values.addValues("a", Arrays.asList("3", "4"));
+        values.addValues("c", Arrays.asList(3, 4));
+        assertEquals(Arrays.asList("1", "3", "4"), values.get("a"));
+        assertEquals(Arrays.asList(3, 4), values.get("c"));
+    }
+
+    @Test
+    public void addValues2() {
+        final Map<String, String[]> vals = new HashMap<>();
+        vals.put("a", new String[]{"1"});
+        vals.put("c", new String[]{"1", "2"});
+        values.addValues(new Values(vals));
+
+        assertEquals(Arrays.asList("1", "1"), values.get("a"));
+        assertEquals(Arrays.asList("1", "2"), values.get("c"));
+    }
+
+    @Test
+    public void setValue() {
+        values.setValue("a", "3");
+        values.setValue("c", "3");
+        assertEquals(Arrays.asList("3"), values.get("a"));
+        assertEquals(Arrays.asList("3"), values.get("c"));
+    }
+
+    @Test
+    public void names() {
+        assertEquals(new HashSet<>(Arrays.asList("a", "b")), values.names());
+    }
+
+    @Test
+    public void asMap() {
+        final Map<String, List<Object>> map = values.asMap();
+        assertEquals(Arrays.asList("1"), map.get("a"));
+        assertEquals(Arrays.asList("1", "2"), map.get("b"));
+    }
+
+}
