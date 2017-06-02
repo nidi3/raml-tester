e603417936d8ae6293cd8fc309e1ac3d4e91ee63
Stefan Niederhauser <ghuder5@gmx.ch>
2017-06-02 10:37:56 +0200

    update license header

--- src/test/java/guru/nidi/ramltester/v10/QueryParameterTest.java
+++ src/test/java/guru/nidi/ramltester/v10/QueryParameterTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2014 Stefan Niederhauser (nidin@gmx.ch)
+ * Copyright © 2014 Stefan Niederhauser (nidin@gmx.ch)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -94,6 +94,12 @@ public class QueryParameterTest extends HighlevelTestBase {
     @Test
     public void validEmptyParam() throws Exception {
         assertThat(test(aggregator, query, get("/query?req"), jsonResponse(200, "\"hula\"")),
+                hasNoViolations());
+    }
+
+    @Test
+    public void queryString() throws Exception {
+        assertThat(test(aggregator, query, get("/queryString?names=a&names=b&age=42"), jsonResponse(200, "\"hula\"")),
                 hasNoViolations());
     }
 }
